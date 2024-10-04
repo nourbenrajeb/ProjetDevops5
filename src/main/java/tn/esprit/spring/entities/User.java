26a5fb3c5e5c4ca9b8d8daa9e0ba4d4e51ffa3bc
nour <nour.benrejeb@esprit.tn>
2024-10-04 11:22:11 +0200

    initialisation du projet timesheet-devops

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -29,6 +29,8 @@ public class User implements Serializable {
 	
 	private String lastName;
 	//private String lastName;
+	//private String lastName;
+
 	
 	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
